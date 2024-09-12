e24de75256540aad90ac0e4adbdc2668f9509d04
Indhirayc <indhirayuantikac1176@gmail.com>
2024-09-12 18:09:11 +0700

    Pertanyaan 1

---
+++ src/P2_Percobaan3_pertanyaan1_13.java
@@ -0,0 +1,30 @@
+public class P2_Percobaan3_pertanyaan1_13 {
+        public static void main(String[] args) {
+            System.out.println("                      KARTU RENCANA STUDI (KRS)");
+            System.out.println("\nNIM            : 244107020171");
+            System.out.println("NAMA           : INDHIRA YUANTIKA CHRISTY");
+            System.out.println("PROGRAM STUDY  : Diploma IV Teknik Informatika");
+            System.out.println("KELAS          : 1C");
+            System.out.println("___________________________________________________________________________");
+            System.out.println("|  KODE MK  |                 MATA KULIAH                 |  SKS  |  JAM  |");
+            System.out.println("___________________________________________________________________________");
+            System.out.println("| RTI241001 | PANCASILA                                   |   2   |   2   |");
+            System.out.println("___________________________________________________________________________");
+            System.out.println("| RTI241002 | Konsep Teknologi Informasi                  |   2   |   4   |");
+            System.out.println("___________________________________________________________________________");
+            System.out.println("| RTI241003 | Critical Thingking dan Problem Solving      |   2   |   4   |");
+            System.out.println("___________________________________________________________________________");
+            System.out.println("| RTI241004 | Matematika Dasar                            |   2   |   6   |");
+            System.out.println("___________________________________________________________________________");
+            System.out.println("| RTI241005 | Rekayasa Perangkat Lunak                    |   2   |   4   |");
+            System.out.println("___________________________________________________________________________");
+            System.out.println("| RTI241006 | Dasar pemprograman                          |   2   |   4   |");
+            System.out.println("___________________________________________________________________________");
+            System.out.println("| RTI241007 | Praktikum Dasar Pemprograman                |   3   |   6   |");
+            System.out.println("___________________________________________________________________________");
+            System.out.println("| RTI241008 | Keselamatan dan Kesehatan Kerja             |   2   |   4   |");
+            System.out.println("___________________________________________________________________________");
+            System.out.println("|           |                    JUMLAH                   |   18  |   34  |");
+            System.out.println("___________________________________________________________________________");
+    }
+}
