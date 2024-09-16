d3b4ff572ee252474b74c95dca79d2e95304d75d
Indhirayc <indhirayuantikac1176@gmail.com>
2024-09-16 12:14:19 +0700

    P3_Percobaan2_PertanyaanNo3

--- src/pertemuan3/Kafe13.java
+++ src/pertemuan3/Kafe13.java
@@ -23,9 +23,10 @@ public class Kafe13 {
         double totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
 
         double nominalBayar = totalHarga - (diskon * totalHarga);
+        int nominalInt = (int) nominalBayar;
 
         System.out.println("Keanggotaan pelanggan: " + keanggotaan);
         System.out.println("Item pembelian: "  + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti ");
-        System.out.println("Nominal bayar Rp " + nominalBayar);
+        System.out.println("Nominal bayar Rp (int) " + nominalBayar);
     }
 }
