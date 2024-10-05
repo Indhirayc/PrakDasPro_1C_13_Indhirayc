ff93a4084620409318a19dcc73572ece5fdd0277
Indhirayc <indhirayuantikac1176@gmail.com>
2024-10-05 21:14:31 +0700

    P6_Percobaan3_pertanyaanNo3

--- src/Pertemuan6/Pemilihan2Percobaan313.java
+++ src/Pertemuan6/Pemilihan2Percobaan313.java
@@ -15,7 +15,7 @@ public class Pemilihan2Percobaan313 {
         System.out.print("Masukan Besarnya Penghasilan : ");
         penghasilan = input13.nextInt(); 
         
-    if (kategori.equalsIgnoreCase("pekerja")) {
+    if (kategori.equals("pekerja")) {
         if (penghasilan <= 2000000)
             pajak = 0.1;
         else if (penghasilan <= 3000000)
@@ -24,7 +24,7 @@ public class Pemilihan2Percobaan313 {
             pajak = 0.2;
         gajiBersih = (int) (penghasilan - (pajak * penghasilan));
         System.out.print("Penghasilan Bersih : " + gajiBersih);
-    } else if (kategori.equalsIgnoreCase("pebisnis")) {
+    } else if (kategori.equals("pebisnis")) {
         if (penghasilan <= 2500000)
             pajak = 0.15;
         else if (penghasilan <= 3500000)
