d464a5fbb0bea08ce109863f3992a6679137f293
Indhirayc <indhirayuantikac1176@gmail.com>
2024-10-05 15:50:37 +0700

    P6_Percobaan1_PertanyaanNo4

--- src/Pertemuan6/Pemilihan2Percobaan113.java
+++ src/Pertemuan6/Pemilihan2Percobaan113.java
@@ -11,12 +11,11 @@ public class Pemilihan2Percobaan113 {
         tahun = input13.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            }   if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
-            else {
-                System.out.println("Bukan Tahun Kabisat");
-            }
-        } else {
+            }   else if ((tahun % 400) ==0) {
+                System.out.println("Tahun Kabisat");
+            } else {
             System.out.println("Bukan Tahun Kabisat");
         }
     }
