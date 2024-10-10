cffc9f0770d975d2da771a5175e7deef201a667a
Indhirayc <indhirayuantikac1176@gmail.com>
2024-10-10 13:25:16 +0700

    P7_Tugas2_13

---
+++ src/Pertemuan7/P7_Tugas2_13.java
@@ -0,0 +1,43 @@
+package Pertemuan7;
+
+import java.util.Scanner;
+
+public class P7_Tugas2_13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int jenis, durasi, total = 0;
+
+        System.out.println("-------------------------------------");
+        System.out.println("===== Masukan Jenis Kendaraan =====");
+        System.out.println("-------------------------------------");
+        System.out.println("1. Mobil");
+        System.out.println("2. Motor");
+        System.out.println("0. Keluar)");
+        System.out.println("-------------------------------------");
+
+        do {
+            System.out.print("Masukan angka dari menu yang dipilih = ");
+            jenis = sc.nextInt();
+            if (jenis == 1 || jenis == 2) {
+                System.out.print("Masukan durasi parkir: ");
+                durasi = sc.nextInt();
+                if (durasi > 5) {
+                    total += 12500;
+                } else if (jenis == 1) {
+                    total += durasi*3000;
+                } else if (jenis == 2) {
+                    total += durasi*2000;
+                } else {
+                    break;
+                }
+                System.out.println("Total biaya parkir: " + total);
+            } else {
+                break;
+            }
+        } while (jenis !=0);
+        
+        System.out.println("Total pembayaran parkir: " + total);
+        sc.close();
+    }
+}
