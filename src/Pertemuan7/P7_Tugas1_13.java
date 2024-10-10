0e4b1ddb9929c406109e49e7801ac7c1a039fae4
Indhirayc <indhirayuantikac1176@gmail.com>
2024-10-10 13:24:47 +0700

    P7_Tugas1_13

---
+++ src/Pertemuan7/P7_Tugas1_13.java
@@ -0,0 +1,38 @@
+package Pertemuan7;
+
+import java.util.Scanner;
+
+public class P7_Tugas1_13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        String namaPelanggan;
+        double hargaTiket = 50000, jmlTiket, totalHarga, diskon;
+
+        do { 
+            System.out.print("Masukan nama pelanggan (ketik 'batal' untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan. ");
+                break;
+            }
+            System.out.print("Masukan Jumlah tiket yang ingin di beli: ");
+            jmlTiket = sc.nextInt();
+            
+            if (jmlTiket > 4) {
+                diskon =  0.1;
+                totalHarga = hargaTiket - (hargaTiket * diskon);
+                System.out.println("Total harga yang harus di bayar: " + totalHarga); 
+            } else if (jmlTiket > 10) {
+                diskon = 0.15;
+                totalHarga = hargaTiket - (hargaTiket * diskon);
+                System.out.println("Total harga yang harus di bayar: " + totalHarga); 
+            } else {
+                diskon = 0;
+                totalHarga = hargaTiket;
+                System.out.println("Total harga yang harus di bayar: " + totalHarga); 
+            } 
+            sc.nextLine();
+        } while (true);
+    }
+}
