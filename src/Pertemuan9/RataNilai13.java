40926d65ba3f03cb1d39991370c42f881474dab0
Indhirayc <indhirayuantikac1176@gmail.com>
2024-10-24 20:18:14 +0700

    P8_Percobaan4

---
+++ src/Pertemuan9/RataNilai13.java
@@ -0,0 +1,23 @@
+package Pertemuan9;
+
+import java.util.Scanner;
+
+public class RataNilai13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int i = 1;
+        while (i<=5){
+            System.out.println("Input Nilai Mahasiswa ke "  + i);
+            float nilaiMhs=0, totalNilai=0, rataNilai=0;
+            for (int j = 1; j<=5; j++) {
+                System.out.print("Nilai ke-" + j + " = ");
+                nilaiMhs = sc.nextInt();
+                totalNilai+=nilaiMhs;
+            }
+            rataNilai = totalNilai/5;
+            System.out.println("Rata-rata Nilai Mahasiswa ke " + i + " adalah " + rataNilai);
+            i++;
+        }
+    }
+}
+
