4a6c778145f3a4d1bac59a17d0ec5f036d2289c8
Indhirayc <indhirayuantikac1176@gmail.com>
2024-10-31 12:27:16 +0700

    P9_Percobaan3_PertanyaanNo1

--- src/Pertemuan10/ArrayRataNilai13.java
+++ src/Pertemuan10/ArrayRataNilai13.java
@@ -9,15 +9,23 @@ public class ArrayRataNilai13 {
         int [] nilaiMhs = new int [10];
         double total = 0;
         double rata2;
+        int jumlahLulus = 0;
+        int totalNilai = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs [i] = sc.nextInt();
+            if (nilaiMhs [i] >= 70) {
+                jumlahLulus++;
+            }
         }
         for (int i = 0; i < nilaiMhs.length; i++){
             total += nilaiMhs[i];
-        }
+        } 
+        
+    
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = " +rata2);
+        System.out.println("Jumlah mahasiswa yang lulus: " + jumlahLulus);
     }
 }
