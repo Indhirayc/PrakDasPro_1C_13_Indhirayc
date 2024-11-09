627b986028ed3f37fa118db6529554543f03e495
Indhirayc <indhirayuantikac1176@gmail.com>
2024-11-09 10:38:26 +0700

    P10_Percoabaan2_PertanyaanNo3

--- src/Pertemuan11/BioskopWithScanner13.java
+++ src/Pertemuan11/BioskopWithScanner13.java
@@ -33,37 +33,27 @@ public class BioskopWithScanner13 {
                     System.out.print("Masukan kolom: ");
                     kolom = sc.nextInt();
                     sc.nextLine();
-                    
+
                     if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2) {
                         System.out.println("Baris dan kolom tidak valid.");
                     } else if (penonton[baris - 1][kolom - 1] != null) {
-                        System.out.println("Kursi pada baris " + baris + " Kolom " + kolom + " sudah terisi. Pilih kursi lain");
+                        System.out.println("Tempat sudah diisi. Input kembali");
                     } else {
                         penonton[baris - 1][kolom - 1] = nama;
                         System.out.println("Data penonton disimpan.");
                     }
+                    
     
-                    System.out.print("Input penonton lainnya? (y/n) : ");
-                    next = sc.nextLine();
+                System.out.print("Input penonton lainnya? (y/n) : ");
+                next = sc.nextLine();
 
                 } while (next.equalsIgnoreCase("y")) ; 
             
             } else if (pilihanMenu == 2) {
                 System.out.println("Daftar Penonton: ");
                 for (int i = 0; i < penonton.length; i++) {
-                    System.out.print("Penonton baris ke-" + (i + 1) + ": ");
-                    for (int j = 0; j < penonton[i].length; j++) {
-                        if (penonton[i][j] == null) {
-                            System.out.print("***");
-                        } else {
-                            System.out.print(penonton[i][j]);
-                        }
-                        if (j < penonton[i].length-1){
-                            System.out.print(", ");
-                        }
-                    }
-                    System.out.println();
-                }
+                    System.out.print("Penonton baris ke-" + (i + 1) + ": " + String.join(", ",penonton[i]));
+                }   
             
             }else if (pilihanMenu == 3){
                 System.out.println("Anda telah keluar dari program");
