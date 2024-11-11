8f6df859590d140653240b7dfd8e7618c29f7e24
Indhirayc <indhirayuantikac1176@gmail.com>
2024-11-11 23:16:03 +0700

    P10_Tugas1_12

--- src/Pertemuan11/Tugas1.java
+++ src/Pertemuan11/Tugas1.java
@@ -8,22 +8,22 @@ public class Tugas1 {
         
         int jumlahResponden = 10;
         int jumlahPertanyaan = 6;
-        int [][] hasilSurvey = new int[jumlahResponden][jumlahPertanyaan];
+        int[][] hasilSurvey = new int[jumlahResponden][jumlahPertanyaan];
 
         System.out.println("========== Input Jawaban Survei ========== ");
         for (int i = 0; i < jumlahResponden; i++) {
-            System.out.println("Responden " + (i+1) + ": ");
+            System.out.println("Responden " + (i + 1) + ": ");
             for (int j = 0; j < jumlahPertanyaan; j++) {
-                System.out.print(" Jawaban Pertanyaan ke-" + (j+1) + ": ");
+                System.out.print(" Jawaban Pertanyaan ke-" + (j + 1) + ": ");
                 hasilSurvey[i][j] = sc.nextInt();
-                
+
                 while (hasilSurvey[i][j] < 1 || hasilSurvey[i][j] > 5) {
                     System.out.print("  Nilai tidak valid. Masukkan nilai antara 1 dan 5: ");
                     hasilSurvey[i][j] = sc.nextInt();
                 }
             }
         }
-        System.out.println("\n=== Rata-rata Nilai Setiap Responden ====");
+        System.out.println("\n========== Rata-rata Nilai Setiap Responden ==========");
         for (int i = 0; i < jumlahResponden; i++) {
             double totalResponder = 0;
             for (int j = 0; j < jumlahPertanyaan; j++) {
@@ -32,8 +32,8 @@ public class Tugas1 {
             double rataResponder = totalResponder / jumlahPertanyaan;
             System.out.println("Rata-rata Responden " + (i + 1) + ": " + rataResponder);
         }
-        System.out.println("\n=== Rata-rata Nilai Setiap Pertanyaan ===");
-        for (int j = 0; j < jumlahResponden; j++) {
+        System.out.println("\n========== Rata-rata Nilai Setiap Pertanyaan ==========");
+        for (int j = 0; j < jumlahPertanyaan; j++) { 
             double totalPertanyaan = 0;
             for (int i = 0; i < jumlahResponden; i++) {
                 totalPertanyaan += hasilSurvey[i][j];
@@ -41,14 +41,14 @@ public class Tugas1 {
             double rataPertanyaan = totalPertanyaan / jumlahResponden;
             System.out.println("Rata-rata Pertanyaan ke-" + (j + 1) + ": " + rataPertanyaan);
         }
-        System.out.println("\n=== Rata-rata Nilai Secara Keseluruhan ===");
-        double totalkeseluruhan = 0;
+        System.out.println("\n========== Rata-rata Nilai Secara Keseluruhan ==========");
+        double totalKeseluruhan = 0;
         for (int i = 0; i < jumlahResponden; i++) {
             for (int j = 0; j < jumlahPertanyaan; j++) {
-                totalkeseluruhan += hasilSurvey [i][j];
+                totalKeseluruhan += hasilSurvey[i][j];
             }
         }
-        double rataKeseluruhan = totalkeseluruhan / (jumlahResponden * jumlahPertanyaan);
+        double rataKeseluruhan = totalKeseluruhan / (jumlahResponden * jumlahPertanyaan);
         System.out.println("Rata-rata Nilai Keseluruhan: " + rataKeseluruhan);
 
     }
