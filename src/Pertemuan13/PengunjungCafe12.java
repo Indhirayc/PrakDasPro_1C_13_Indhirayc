55ea60146cabb1e6ac292ce4216451a7c12a1b36
Indhirayc <indhirayuantikac1176@gmail.com>
2024-11-27 19:20:34 +0700

    P13_Percobaan4_PertanyaanNo2

--- src/Pertemuan13/PengunjungCafe12.java
+++ src/Pertemuan13/PengunjungCafe12.java
@@ -6,8 +6,8 @@ public class PengunjungCafe12 {
     }
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String pengunjung : namaPengunjung) {
+            System.out.println("- " + pengunjung);
         }
     }
 }
