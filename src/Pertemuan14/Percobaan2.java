a08fa7226897a549e0e59eb5d49e2ae7aa250b05
Indhirayc <indhirayuantikac1176@gmail.com>
2024-12-09 21:43:42 +0700

    P14_Percobaan2_PertanyaanNo2

--- src/Pertemuan14/Percobaan2.java
+++ src/Pertemuan14/Percobaan2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Percobaan2 {
     static String deret = "";
+    
     static int hitungPangkat (int x, int y) {
         if (y == 0) {
             deret += "1";
@@ -11,19 +12,17 @@ public class Percobaan2 {
         } else {
             deret += x + "x";
             return (x * hitungPangkat(x, y - 1 ));
-            
         }
     }
     public static void main(String[] args) {
         Scanner indhira12 = new Scanner (System.in);
+        int bilangan, pangkat;
         System.out.print("Bilangan yang  dihitung: ");
-        int bilangan = indhira12.nextInt();
+        bilangan = indhira12.nextInt();
         System.out.print("Pangkat: ");
-        int pangkat = indhira12.nextInt();
+        pangkat = indhira12.nextInt();
 
-        deret = "";
         int hasil = hitungPangkat(bilangan, pangkat);
-
         System.out.println(deret + " = " + hasil);
     }
 }
